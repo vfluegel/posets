727e6e788259487269ccc4dd27243c6fd937d654
Vanessa Fluegel <vanessa.fluegel@gmx.de>
2024-11-26 20:32:25 +0100

    Added forest as static unique_ptr in st_backed

--- include/posets/downsets/st_backed.hh
+++ include/posets/downsets/st_backed.hh
@@ -27,48 +27,51 @@ namespace posets::downsets {
     size_t dim;
 
     size_t root{};
-    static utils::sforest<V> get_sforest() {
-        static utils::sforest<V> f(k, dim);
-        return f;
-    }
+    static std::unique_ptr<utils::sforest<V>> forest{};
 
   public:
+    static utils::sforest<V>* get_sforest() {
+      return forest.get();
+    }
+
     st_backed() = delete;
 
     st_backed (std::vector<V>&& elements, unsigned k=0) 
-        : k {}
-        , dim {elements.begin().size()} 
     {
-        get_sforest().add_vectors(std::move (elements));
+      if(!forest) {
+        forest = std::make_unique<utils::sforest<V>>(k, elements.begin().size());
+      }
+      get_sforest()->add_vectors(std::move (elements));
     }
 
     st_backed (V&& v, unsigned k=0) 
-        : k {}
-        , dim {v.size()} 
     {
-        get_sforest().add_vectors(std::array<V, 1> { std::move (elements) });
+      if(!forest) {
+        forest = std::make_unique<utils::sforest<V>>(k, v.size());
+      }
+      get_sforest()->add_vectors(std::array<V, 1> { std::move (v) });
     }
 
     bool contains (const V& v) const {
-        return this->f.cover_vector(this->root, v);
+        return get_sforest()->cover_vector(this->root, v);
     }
 
     // Union in place
     void union_with (st_backed&& other) {
-        size_t newRoot = f.st_union(this->root, other.root);
+        size_t newRoot = get_sforest()->st_union(this->root, other.root);
         this->root = newRoot;
     }
 
     // Intersection in place
     void intersect_with (const kdtree_backed& other) {
-        size_t newRoot = f.st_intersect(this->root, other.root);
+        size_t newRoot = get_sforest()->st_intersect(this->root, other.root);
         this->root = newRoot;
     }
   };
 
   template <Vector V>
   inline std::ostream& operator<<(std::ostream& os, const st_backed<V>& f) {
-    f.get_sforest().print_children(f.root, 0);
+    f.get_sforest()->print_children(f.root, 0);
     os << std::endl;
     return os;
   }
