2e2f78eb0fe4f39318ecf128fa38440022a4279a
gaperez64 <gaperez64@gmail.com>
2024-12-14 22:58:22 +0100

    why did this not break locally?

--- include/posets/downsets.hh
+++ include/posets/downsets.hh
@@ -9,6 +9,7 @@
 #include <posets/downsets/vector_backed_one_dim_split.hh>
 #include <posets/downsets/vector_backed_one_dim_split_intersection_only.hh>
 #include <posets/downsets/vector_or_kdtree_backed.hh>
+#include <posets/downsets/sharingtree_backed.hh>
 #include <posets/vectors.hh>
 #include <posets/vectors/traits.hh>
 
